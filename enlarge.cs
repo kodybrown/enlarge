ec02b23b4009e6ffaa71354e9426e2ab0e341c90
Kody Brown <kody.brown@venafi.com>
2012-09-20 20:46:38 -0600

    - Initial commit.

---
+++ enlarge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bricksoft.PowerCode;
+
+namespace Bricksoft.DosToys
+{
+	public class enlarge
+	{
+		public static void Main( string[] args )
+		{
+			// Adjust the window size.
+			Console.WindowWidth = Console.LargestWindowWidth - 4;
+			Console.WindowHeight = Console.LargestWindowHeight - 1;
+
+			Console.BufferWidth = Console.WindowWidth;
+			Console.BufferHeight = 3000;
+
+			ConsoleUtils.CenterWindow();
+		}
+	}
+}
